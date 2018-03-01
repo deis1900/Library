ce35d96b2d62065c928ea9908875429534f70e3a
denis <deis1900@i.ua>
2018-03-01 19:12:26 +0200

    Fix Validation, fix sqlExeption with ' character

--- src/main/java/Selection.java
+++ src/main/java/Selection.java
@@ -4,23 +4,21 @@ import Input.Validator;
 
 import java.sql.SQLException;
 
-class Menu {
+class Selection {
     private Action action = new Action();
     private Validator valid = new Validator();
 
     void choice(String str) throws SQLException {
 
-        String[] strings = str.split(" -", 2);
-// Input should be correct for the name of table
+        int amountOfDate = (str.length() - str.replace(" -", " ").length()) +1;
+        String[] strings = str.split(" -", amountOfDate);
 
         if (valid.verifyNameOfTable(strings[0])) {
 
             String section = strings[0].toUpperCase();
-            strings = str.split(" -", 5);
             String commandForAction = strings[1].toUpperCase();
 
-// I filter the method list
-            if (valid.filterCommandList(commandForAction)) {
+            if (amountOfDate == 2) {
                 Model model = new Model(strings[2], strings[3], strings[4]);
                 action.actionMake(section, commandForAction, model);
 
